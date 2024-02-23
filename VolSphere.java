41392833ab3d8ea2f6d0afabd309686440a93799
Aidan Harvey <aidan.harvey@stu.ocsb.ca>
2024-02-23 22:01:06 +0000

    Version 1.1 of VolSphere.java, added & completed base code.

---
+++ VolSphere.java
@@ -0,0 +1,48 @@
+
+/***
+
+ Program for calculating the radius of a sphere.
+
+* @author  Aidan.
+
+* @version 1.1
+
+* @since   02-23-2024
+
+*/
+import java.util.Scanner;
+//Imports the scanner
+
+public class VolSphere {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //Creates a new scanner object to be used for user input
+        System.out.println("Please enter the radius of a sphere (in CM):");
+        //Prompts the user to enter the value for a radius
+        double sphereRadius = sc.nextDouble();
+        //Allows the user to input the radius of their sphere
+        if (sphereRadius == 0d) {
+            System.out.println("Please enter a valid (Positive) integer.");
+            //Asks the user to input a valid number
+        }
+            else if (sphereRadius < 0d) {
+            System.out.println("Please enter a valid (Positive) integer.");
+            //Prompts the user to input a valid number
+        }
+        else {
+            double volume = 4.0 / 3.0 * Math.PI * Math.pow(sphereRadius, 3);
+            //This will run the calculations for the volume of the sphere
+            System.out.println("The volume of the Sphere is: " + (Math.round(volume*100.0))/100.0);
+            //Prints the calculated volume of the sphere
+        }
+
+        
+
+
+        sc.close();
+
+    }
+    
+}
